13d04a46d00c33384680347a63629c7c55bfc4d0
Jason Lafferty <jason@jasonlafferty.com>
2017-11-21 08:50:17 +0000

    Putting content in language files.

--- app/js/l10n/en/common.js
+++ app/js/l10n/en/common.js
@@ -1,3 +1,18 @@
 l10n.en = {
-	header: "Cake app"
+	header: "Cake app",
+	label: {
+		name: "Name",
+		comment: "Comment",
+		imageUrl: "Image URL",
+		yumFactor: "Yum factor",
+		id: "ID"
+	},
+	error: {
+		name: "Please enter a name for your cake.",
+		comment: "Please enter a comment for your cake.",
+		imageUrl: "Please enter a valid image URL for your cake.",
+		yumFactor: "Please enter a yum factor between 1 and 5."
+	},
+	backLink: "Back to all the yummy cakes",
+	addLink: "Add a new cake"
 };
